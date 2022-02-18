3f0195b9dab8ee42caa64e5dc6c5d9eee6c3eb54
qorganbek <98808957+qorganbek@users.noreply.github.com>
2022-02-18 21:36:32 +0600

    Delete 10.py

--- 10.py
+++
@@ -1,9 +0,0 @@
-def filter_duplicate(a):
-    b = []
-    for i in a:
-        if i not in b:
-            b.append(i)
-    return b
-a = list(map(int,input().split()))
-print(filter_duplicate(a))
-         
