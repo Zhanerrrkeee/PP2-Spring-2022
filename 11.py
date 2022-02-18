4c9c3d57496b1e7ffbbafb25f47c2dbfc3bab4c3
qorganbek <98808957+qorganbek@users.noreply.github.com>
2022-02-18 21:40:16 +0600

    Delete 11.py

--- 11.py
+++
@@ -1,9 +0,0 @@
-def CheckPalindrome(s):
-    for i in range(int(len(s)/2)):
-        if s[i] != s[len(s)-i-1]:
-            return False
-    return True
-if CheckPalindrome(input()):
-    print("Yes")
-else : print("No")
-        
