b802a2ff8f6e0d34cc26a8f89fe29542f7b1798b
qorganbek <98808957+qorganbek@users.noreply.github.com>
2022-02-18 21:40:34 +0600

    Delete 3.py

--- 3.py
+++
@@ -1,6 +0,0 @@
-
-def solve(numheads, numlegs):
-    chicken = (4 * numheads - numlegs)/2
-    rabbit = numheads - chicken
-    print(f'Chickens - {int(chicken)} and Rabbits - {int(rabbit)}')
-solve(int(input()),int(input()))    
