d4f5be2941bffca6a8fc227a6d24a18cd81a3020
qorganbek <98808957+qorganbek@users.noreply.github.com>
2022-02-18 21:46:44 +0600

    Add files via upload

---
+++ lab3/1.py
@@ -0,0 +1,12 @@
+class ThisString():
+
+    def get_String(self):
+        self.s = input()
+
+    def print_String(self):
+        print(self.s.upper())
+s1 = ThisString()
+s1.get_String()
+s1.print_String()
+
+
